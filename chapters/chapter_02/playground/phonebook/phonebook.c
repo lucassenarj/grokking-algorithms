55b970846fd325b9923ab3d12874e88327d3f7a8
Lucas Sena <lucassena.rj@gmail.com>
2024-01-31 20:54:32 +0000

    refactor(playground): Improve phonebook to use malloc

--- chapters/chapter_02/playground/phonebook/phonebook.c
+++ chapters/chapter_02/playground/phonebook/phonebook.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 // define struct to each person
 typedef struct {
@@ -15,7 +16,7 @@ typedef struct {
     char phone[10];
 } Person;
 
-Person *selection_sort(Person people[], int size);
+Person *selection_sort(Person **people, int size);
 
 int main(void) {
     // open file in read mode
@@ -31,14 +32,16 @@ int main(void) {
     fscanf(csv, "%d\n", &length);
 
     // create list os people
-    Person people[length];
+    Person *people[length];
+    Person *p = malloc(sizeof(Person));
 
     for (i = 0; i < length; i++) {
-        Person p;
-        fscanf(csv, "%d,", &p.id);
-        fscanf(csv, "%49[^,]", p.name);
-        fscanf(csv, ",%9[^\n]", p.phone);
+
+        fscanf(csv, "%d,", &p->id);
+        fscanf(csv, "%49[^,]", p->name);
+        fscanf(csv, ",%9[^\n]", p->phone);
         people[i] = p;
+        p = malloc(sizeof(Person));
     }
 
     // close file
@@ -51,28 +54,29 @@ int main(void) {
     printf("\n\tList of people: \n");
     for (i = 0; i < length; i++)
     {
-        printf("%s, %s\n", people[i].name, people[i].phone);
+        printf("%s, %s\n", people[i]->name, people[i]->phone);
     }
+    free(p);
     //*/
 }
 
-Person *selection_sort(Person people[], int size)
+Person *selection_sort(Person **people, int size)
 {
     int i, j;
 
     for (i = 0; i < size; i++)
     {
-        Person p = people[i];
+        Person *p = people[i];
         int minIndex = i;
         for (j = 0; j < size; j++)
         {
-            if(strcasecmp(people[j].name, p.name) >= 0) {
+            if(strcasecmp(people[j]->name, p->name) >= 0) {
                 p = people[j];
                 minIndex = j;
             }
             // if min index is smaller then current index, means that current index isen't the smaller anymore
             if (minIndex != i) {
-                Person tmp = people[i];
+                Person *tmp = people[i];
                 people[i] = people[minIndex];
                 people[minIndex] = tmp;
             }
@@ -80,5 +84,5 @@ Person *selection_sort(Person people[], int size)
         }
     }
     
-    return people;
+    return *people;
 }
